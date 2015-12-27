edab730a0339fb6ddad36c42c93881dc8398e14d
VadymVolos <vadymvolos@gmail.com>
2015-12-27 19:21:53 +0200

    1

---
+++ Fifteen-Child-One/forum.php
@@ -0,0 +1,49 @@
+<?php
+
+// Make sure no one attempts to run this view directly.
+if (!defined('FORUM'))
+	exit;
+
+?>
+</div>
+<div class="container">
+	<div class="row forumview">
+		<div class="col-sm-3">
+			<div class="list-group list-group-forum">
+				<a class="list-group-item" href="index.php"><span class="fa fa-fw fa-chevron-left"></span> <?php _e('Back to index', 'luna') ?></a>
+			</div>
+			<hr />
+			<?php if ((is_subforum($id) && $id != '0')): ?>
+				<h5 class="list-group-head"><?php _e('Subforums', 'luna') ?></h5>
+				<div class="list-group list-group-forum">
+					<?php draw_subforum_list('forum.php') ?>
+				</div>
+				<hr />
+			<?php endif; ?>
+			<div class="forum-list hidden-xs">
+				<div class="list-group list-group-forum">
+					<?php draw_forum_list('forum.php', 1, 'category.php', '') ?>
+				</div>
+				<hr />
+			</div>
+			<div class="list-group list-group-forum">
+				<?php draw_mark_read('list-group-item', 'forumview') ?>
+				<?php if ($id != '0' && $is_admmod) { ?>
+					<a class="list-group-item" href="backstage/moderate.php?fid=<?php echo $forum_id ?>&p=<?php echo $p ?>"><span class="fa fa-fw fa-eye"></span> <?php _e('Moderate forum', 'luna') ?></a>
+				<?php } ?>
+			</div>
+			<div class="visible-xs-block"><hr /></div>
+		</div>
+		<div class="col-sm-9">
+			<div class="list-group list-group-thread">
+				<div class="jumbotron jumbotron-forum_php-margin_bottom" style="background-color: <?php echo $cur_forum['color']; ?>;">
+					<div class="container">
+						<h2 class="forum-title"><?php echo $faicon.luna_htmlspecialchars($cur_forum['forum_name']) ?></h2><span class="pull-right naviton"><?php echo $paging_links ?><?php echo $comment_link ?></span>
+						<div class="forum-desc"><?php echo $cur_forum['forum_desc'] ?></div>
+					</div>
+				</div>
+			
+				<?php draw_threads_list(); ?>
+			</div>
+		</div>
+	</div>
---
+++ Fifteen-Child-One/information.php
@@ -0,0 +1,26 @@
+<?php
+$xmlstr = <<<XML
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE extension SYSTEM "ext-1.0.dtd">
+
+<!--
+/*
+ * Copyright (C) 2001-5020 Vadym Volos - Webstudio11
+ * Licensed - use but not to sell.
+ */
+-->
+
+<style engine="1.0">
+	<id>fifteen-child-one</id>
+	<name>Fifteen-Child-One</name>
+	<developer>Vadym Volos - Webstudio11</developer>
+	<date>27 December 2015</date>
+	<version>1.0.0</version>
+	<minversion>1.3.0</minversion>
+	<maxversion>1.3.0</maxversion>
+	<description>Fifteen is the default theme of the Luna. 
+	1) Added scrollbar (for browsers Chrome, Firefox, Opera). 2) Added in the left sidebar color headers. 3) Changed header threads.</description>
+	<parent_theme>Fifteen</parent_theme>
+</style>
+XML;
+?>
