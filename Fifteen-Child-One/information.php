b9f0a71d75ecc773d543a143a6ca3710c461628d
VadymVolos <vadymvolos@gmail.com>
2016-05-10 15:18:49 +0300

    Updated to Luna 2.0.0
    Child theme - need for analytics code.

--- Fifteen-Child-One/information.php
+++ Fifteen-Child-One/information.php
@@ -5,7 +5,7 @@ $xmlstr = <<<XML
 
 <!--
 /*
- * Copyright (C) 2001-5020 Vadym Volos - Webstudio11
+ * Copyright (C) 2001-3020 Vadym Volos - Webstudio11
  * Licensed - use but not to sell.
  */
 -->
@@ -14,12 +14,12 @@ $xmlstr = <<<XML
 	<id>fifteen-child-one</id>
 	<name>Fifteen-Child-One</name>
 	<developer>Vadym Volos - Webstudio11</developer>
-	<date>27 December 2015</date>
+	<date>05 May 2016</date>
 	<version>1.0.0</version>
-	<minversion>1.3.0</minversion>
-	<maxversion>1.3.0</maxversion>
+	<minversion>2.0.0</minversion>
+	<maxversion>2.0.0</maxversion>
 	<description>Fifteen is the default theme of the Luna. 
-	1) Added scrollbar (for browsers Chrome, Firefox, Opera). 2) Added in the left sidebar color headers. 3) Changed header threads.</description>
+	1) Added scrollbar (for browsers Chrome, Firefox, Opera) 2) Changed header threads.</description>
 	<parent_theme>Fifteen</parent_theme>
 </style>
 XML;
